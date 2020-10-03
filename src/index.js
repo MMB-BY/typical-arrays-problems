9ba330045c9ed1087f54edc1f7f6fb349c875a59
MMB-BY <mmbmainp@gmail.com>
2020-10-03 13:48:30 +0300

    init: start typical-arrays-problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,59 @@
-
 exports.min = function min (array) {
-  return 0;
+    let result = 0;
+    try {
+    if (array[0] / 1 !== array[0]) {
+        return 0;
+    }
+        result = array[0];
+    for ( let i=0; i < array.length; i++){
+        if (array[i] < result){
+            result = array[i];
+        }
+        else{
+            continue;
+        }
+    }
+  return result;}
+  catch{
+      return 0;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+    let result = 0;
+    try {
+    if (array[0] / 1 !== array[0]) {
+        return 0;
+    }
+        result = array[0];
+    for ( let i=0; i < array.length; i++){
+        if (array[i] > result){
+            result = array[i];
+        }
+        else{
+            continue;
+        }
+    }
+  return result;}
+  catch{
+      return 0;
+  }
 }
 
+
 exports.avg = function avg (array) {
-  return 0;
+    let result = 0;
+    try {
+        if (array[0] / 1 !== array[0]) {
+            return 0;
+        }
+        for ( let i=0; i < array.length; i++){
+            result += +array[i];
+        }
+        result = result/array.length;
+        return result;
+    }
+  catch{
+      return 0;
+  }
 }
